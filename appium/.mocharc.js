8f3eaf14fe2f2fe9f755574334a74450b952c1cb
anugondasriharini <anugondasriharini@gmail.com>
2026-08-05 11:44:22 +0530

    fix: add exit flag to mocha config to prevent hanging

--- appium/.mocharc.js
+++ appium/.mocharc.js
@@ -2,4 +2,5 @@ module.exports = {
   spec: 'tests/appium-mobile.test.js',
   timeout: 120000,
   reporter: 'mochawesome',
+  exit: true,
 };
--- selenium/.mocharc.js
+++ selenium/.mocharc.js
@@ -2,4 +2,5 @@ module.exports = {
   spec: 'tests/selenium-web.test.js',
   timeout: 120000,
   reporter: 'mochawesome',
+  exit: true,
 };
